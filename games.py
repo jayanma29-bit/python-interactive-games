582c1d03e8a881253899c703e361172c95429eff
Jayan Akabari <jayanma29@gmail.com>
2026-05-26 13:01:52 -0500

    games.py

--- games.py
+++ games.py
@@ -1,6 +1,6 @@
-import math 
+import math
 import random 
-
+import time
 
 print("-----------------Choose a game!-----------------")
 
@@ -41,19 +41,6 @@ def game_caesar():
     else:
         print("\nInvalid operation.")
 
-
-    while True:
-        try:
-            number = int(input("Enter a number to check if it's even or odd: "))
-            if number % 2 == 0:
-                print(f"{number} is even.\n")
-            else:
-                print(f"{number} is odd.\n")
-            break
-        except ValueError:
-            print("Please enter a valid integer.\n")
-
-
 def numb_guess():
 
     import random
@@ -89,30 +76,9 @@ def game_even_odd():
                 print(f"{number} is odd.\n")
             break
         except ValueError:
-            print("Please enter a valid integer.\n")
+            print("Please enter a valid integer (A number).\n")
 
-def cordinate_dif():
-    def distance(first_point, second_point):
-        x_diff = second_point[0] - first_point[0]
-        y_diff = second_point[1] - first_point[1]
-        sum_of_squares = pow(x_diff, 2) + pow(y_diff, 2)
-        return math.sqrt(sum_of_squares)
-
-    print("Enter coordinates as x, y (e.g., 3, 4): ")
-    p1_input = input("Point 1: ").split(",")
-    p2_input = input("Point 2: ").split(",")
-
-    point1 = (float(p1_input[0]), float(p1_input[1]))
-    point2 = (float(p2_input[0]), float(p2_input[1]))
-
-    result = distance(point1, point2)
-    rounded = round(result, 3)
-    print(f"The distance is: {rounded}")
-
-
-def game_of_pig():
-    import random
-
+def game_of_pig(): 
     total_score = 0
     computer_score = 0
     turn_number = 1
@@ -189,15 +155,101 @@ def game_of_pig():
         print("The computer won. Better luck next time!")
     else:
         print("It's a tie!")
+#BETA!!!
+def cord_diff():
+    def distance(first_point, second_point):
+        x_diff = second_point[0] - first_point[0]
+        y_diff = second_point[1] - first_point[1]
+        sum_of_squares = pow(x_diff, 2) + pow(y_diff, 2)
+        return math.sqrt(sum_of_squares)
 
+    print("Enter coordinates as x, y (e.g., 3, 4): ")
+    p1_input = input("Point 1: ").split(",")
+    p2_input = input("Point 2: ").split(",")
+
+    point1 = (int(p1_input[0]), int(p1_input[1]))
+    point2 = (int(p2_input[0]), int(p2_input[1]))
+
+    result = distance(point1, point2)
+    rounded = round(result, 3)
+    print(f"The distance is: {rounded}")
+
+def tic_tac_toe():
+        
+    def get_valid_index(prompt):
+        while True:
+            try:
+                index = int(input(prompt))
+                index -= 1
+                if index >= 0 and index <= 2:
+                    return index
+                print("Must be 0 - 2 inclusive!")
+            except ValueError:
+                print("Must be an integer!")
+
+    def game_is_over(board):
+        for i in range(3):
+            if board[i][0] == board[i][1] == board[i][2] and board[i][0] != " ":
+                print_board(board)
+                print(board[i][0] + " wins!")
+                return True
+            
+            if board[0][i] == board[1][i] == board[2][i] and board[0][i] != " ":
+                print_board(board)
+                print(board[0][i] + " wins!")
+                return True
+            
+        if board[0][0] == board[1][1] == board[2][2] and board[0][0] != " ":
+            print_board(board)
+            print(board[0][0] + " wins!")
+            return True
+        
+        if board[2][0] == board[1][1] == board[0][2] and board[2][0] != " ":
+            print_board(board)
+            print(board[2][0] + " wins!")
+            return True
+        
+        if " " not in board[0] and " " not in board[1] and " " not in board[2]:
+            print_board(board)
+            print("Tie game!")
+            return True
+        
+        return False
+        
+    def print_board(board):
+        for row in board:       
+            print(row)
+
+    board = [                   
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+        [' ', ' ', ' '],
+    ]
+    turn = "x"
+    while not game_is_over(board):
+        print_board(board)
+        print("\nIt's " + turn + "'s turn.")
+        row = get_valid_index("\n\nRow: ")
+        col = get_valid_index("\nCol: ")
+        print("\n")
+        
+
+        if board[row][col] != " ":
+            print("That space is taken!")
+        else:
+            board[row][col] = turn
+            if turn == "x":
+                turn = "o"
+            else:
+                turn = "x"
 
 def post_game_menu():
+    time.sleep(5)
     print("\nWhat would you like to do next?\n")
     print("(1) Play the same game again\n")
     print("(2) Choose a different game\n")
     print("(3) End the game\n")
-    return input("Enter 1, 2, 3, or 3: ").strip()
-
+    return input("Enter 1, 2, or 3: ").strip()
 
 def game_choice():
     last_game = None
@@ -207,9 +259,10 @@ def game_choice():
             user_input = input(
                 "(1.) Caesar Encryptor\n"
                 "(2.) Number Guessing\n"
-                "(3.) Even or Odd\n"
-                "(4.) Cordinate Differece\n" \
-                "(5.) Game of Pig\n\n"
+                "(3.) Even or Odd\n" \
+                "(4.) Game of Pigs\n" \
+                "(5.) Cordinate Difference\n"
+                "(6.) Tic Tac Toe\n\n"
                 "Enter the number: ").strip().lower()
 
             if user_input in ["1", "one"]:
@@ -223,14 +276,16 @@ def game_choice():
             elif user_input in ["3", "three"]:
                 print("\nYou have chosen the Even or Odd game!\n")
                 last_game = game_even_odd
-            
             elif user_input in ["4", "four"]:
-                print("\nYou have chosen the cordinate difference!\n")
-                last_game = cordinate_dif
-
-            elif user_input in ["5", "five"]:
-                print("\nYou have chosen the Game of Pig!!\n")
+                print("\nYou have chosen the Game of Pigs!" )
                 last_game = game_of_pig
+            elif user_input in ["5", "five"]:
+                print("\nYou have chosen the Cordinate difference!" )
+                last_game = cord_diff
+            elif user_input in ["6", "six"]:
+                last_game = tic_tac_toe
+
+                
 
             else:
                 print("Invalid choice!")
